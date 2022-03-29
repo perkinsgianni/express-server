ea308e89cd5c39900f8cdfe1e182fd5240a07558
Gianni P <perkinsgianni@gmail.com>
2022-03-29 11:28:08 -0400

    Add README.md, update .gitignore, add code to server/app.js

--- server/app.js
+++ server/app.js
@@ -1,10 +1,32 @@
 // import files and packages up here
 
+var express = require('express');
+var morgan = require('morgan');
+var jsonData = require('./data.json');
+var bodyParser = require("body-parser");
 
 // create your express server below
-var app;
+
+var app = express();
 
 // add your routes and middleware below
 
+// middleware that uses morgan's dev format for each log request
+app.use(morgan('dev'))
+// middleware that only parses json
+app.use(bodyParser.json());
+
+// server should respond to GET requests to / route
+app.get('/', (req, res) => {
+    // server should respond with a status code of 200
+    res.status(200).send("It's working")
+  });
+
+// server should respond to GET requests to data/ route with the top spots data
+app.get('/data', (req, res) => {
+    res.status(200).send(jsonData)
+  });
+
 // finally export the express application
+
 module.exports = app;
